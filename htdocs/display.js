c4fb5a27edb4b4d2b276c3607e91665cf851aaca
Jim Crossley <jim@crossleys.org>
2010-03-13 12:00:43 -0500

    More sensible positioning; interesting fading

--- htdocs/display.js
+++ htdocs/display.js
@@ -3,22 +3,23 @@ function Display (quotes, div) {
   this.quotes = this.shuffle(quotes);
   this.index = 0;
   this.area = div;
-  this.area.hide();
+  this.area.children().hide();
 }
 
 Display.prototype = {
 
   show: function() {
     var x = this.next();
-    this.area.html(x.quote+"<br/><br/> -- <i>"+x.author+"</i>").fadeIn(1000);
+    this.area.find('#quote').html(x.quote).fadeIn(1000);
+    this.area.find('#author').html(x.author).fadeIn(3000);
     var self = this;
     window.setTimeout(function() { self.hide(); }, 3000+x.quote.length*30);
   },
 
   hide: function() {
-    this.area.fadeOut(2000);
+    this.area.children().fadeOut(2500);
     var self = this;
-    window.setTimeout(function() { self.show(); }, 2000);
+    window.setTimeout(function() { self.show(); }, 3000);
   },
 
   next: function() {
